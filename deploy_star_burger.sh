5c20c293a3d127bd5dec367e6ed6deb621d87bb3
ludwig300 <mr4silent@gmail.com>
2023-07-18 23:24:28 +0700

    Updated deploy_star_burger.sh

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-ROLLBAR_TOKEN=your_rollbar_token
+ROLLBAR_TOKEN=8729612243494072bb65a777c8acee76
+PROJECT_NAME=star-burger
 
 set -Eeuo pipefail
 cd /opt/star-burger/
 source venv/bin/activate
 git reset --hard HEAD
 git pull origin master
-revision=$(git rev-parse --short HEAD)
+revision=$(git rev-parse HEAD)
+author=$(git --no-pager show -s --format='%an <%ae>' $revision)
+comment=$(git log -1 --pretty=%B)
+
 pip install -r requirements.txt
 npm ci --include=dev
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 python3 ./manage.py collectstatic --noinput
-python3 ./manage.py migrate
+python3 ./manage.py migrate --noinput
 
-sudo systemctl restart gunicorn
 sudo systemctl restart star-burger
 
-http POST https://api.rollbar.com/api/1/deploy/ access_token=$ROLLBAR_TOKEN environment=production revision=$revision local_username=`whoami`
+http POST https://api.rollbar.com/api/1/deploy/ access_token=$ROLLBAR_TOKEN environment=production revision=$revision local_username=`whoami` rollbar_name=$author comment="$comment" project_name=$PROJECT_NAME
 
 echo "Deploy succeeded"
